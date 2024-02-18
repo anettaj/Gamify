178f28e1d2327b0e3e4b2a230255b7e384caa945
anettaj <58222128+anettaj@users.noreply.github.com>
2024-02-19 01:02:48 +0530

    ScrollableGamesWidget added

--- lib/Pages/Home.dart
+++ lib/Pages/Home.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:gamify/data.dart';
+
+import '../widget/scrollable_games_widget.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -8,10 +11,177 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  var H;
+  var W;
+  var _selectedGame;
+
+  @override
+  void initState(){
+    super.initState();
+    _selectedGame=0;
+  }
   @override
   Widget build(BuildContext context) {
+    H=MediaQuery.of(context).size.height;
+    W=MediaQuery.of(context).size.width;
     return Scaffold(
-      body: Column(),
+      body: Stack(
+        children: [
+          featuredGameWidget(),
+          gradiantBoxWidget(),
+          topLayerWidget()
+
+        ],
+      )
     );
   }
-}
+
+  Widget featuredGameWidget(){
+    return SizedBox(
+      height: H*0.5,
+      width: W,
+      child:PageView(
+        onPageChanged: (_index){
+          setState(() {
+            _selectedGame=_index;
+            print("index:$_index");
+          });
+        },
+        scrollDirection: Axis.horizontal,
+        children: featuredGames.map((_game){
+          return Container(
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: NetworkImage(_game.coverImage.url)
+                  )
+              ));
+        }).toList(),)
+        
+    );
+  }
+
+
+  Widget gradiantBoxWidget(){
+    return Align(
+      alignment: Alignment.bottomCenter,
+      child: Container(
+        height: H*0.80,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+              colors:[
+                Color.fromRGBO(35, 45, 59, 1.0),
+                Colors.transparent
+
+              ],
+              stops: [0.65,1.0],
+              begin: Alignment.bottomCenter,
+              end: Alignment.topCenter
+          )
+        ),
+      ),
+    );
+  }
+
+  Widget topLayerWidget() {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: H *0.05,vertical: H*0.005 ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          _topBarWidget(),
+          SizedBox(
+            height: H*0.15,
+          ),
+          _featuredGameInfoWidget(),
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: H *0.01),
+            child: ScrollableGamesWidget(height: H*0.24,width: W,showTitle: true,gameData: games,),
+          )
+        ],
+      ),
+    );
+  }
+    Widget _topBarWidget(){
+      return SizedBox(
+        height: H*0.13,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Icon(Icons.menu,
+                color: Colors.white,
+                size: 30,
+              ),
+              Row(
+                children: [
+                  Icon(Icons.search,
+                    color: Colors.white,
+                    size: 30,
+                  ),
+                  SizedBox(
+                    width: W *0.03,
+                  ),
+                  Icon(Icons.notifications,
+                    color: Colors.white,
+                    size: 30,
+                  ),
+                ],
+              )
+            ],
+          ),
+      );
+    }
+    Widget _featuredGameInfoWidget(){
+    return SizedBox(
+      height: H*0.14,
+      width: W,
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+
+          Text(featuredGames[_selectedGame].title,
+
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: H*0.040,
+            fontWeight: FontWeight.bold
+          ),
+            maxLines: 2,
+          ),
+          SizedBox(
+            height: H*0.01,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisSize: MainAxisSize.max,
+            children: featuredGames.map((_games){
+              bool _isActive=_games.title  == featuredGames[_selectedGame].title;
+              double _circleRadius=H*0.004;
+              return Container(
+                margin: EdgeInsets.only(
+                  right: W *0.010,
+                ),
+                height: _circleRadius*2,
+                width: _circleRadius*2,
+                decoration: BoxDecoration(
+                  color: _isActive? Colors.blue:Colors.grey,
+                  borderRadius: BorderRadius.circular(100)
+                ),
+              );
+            }).toList(),
+          )
+
+
+        ],
+      ),
+    );
+    }
+
+
+  }
---
+++ lib/widget/scrollable_games_widget.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:gamify/data.dart';
+class ScrollableGamesWidget extends StatelessWidget {
+  final double height;
+  final double width;
+  final bool showTitle;
+  final List<Game> gameData;
+
+   ScrollableGamesWidget({
+    Key?key,
+    required this.height,
+    required this.width,
+    required this.showTitle,
+    required this.gameData
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: height,
+      width: width,
+      child: ListView(
+        physics: BouncingScrollPhysics(),
+        scrollDirection: Axis.horizontal,
+        children: gameData.map((_game){
+          return Container(
+            height: height,
+            width: width*0.30,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              mainAxisSize: MainAxisSize.max,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  padding: EdgeInsets.only(right: width*0.03),
+                  height: height*0.80,
+                  width: width*0.45,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(5),
+                    image: DecorationImage(
+                        image: NetworkImage(_game.coverImage.url),
+                      fit: BoxFit.cover
+                    )
+                  ),
+                ),
+                Text(_game.title,
+                  maxLines: 2,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: height*0.08
+                  ),
+                )
+              ],
+            ),
+          );
+        }).toList(),
+      ),
+    );
+  }
+}
